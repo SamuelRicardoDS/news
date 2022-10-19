c8ffba2863b93c327eb75af6dd2aff002b733cac
SamuelRicardoDS <samuelricardods@gmail.com>
2022-10-19 19:21:40 -0300

    static site generation for perfo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import { SubscribeButton } from "../components/SubscribeButton";
 
 import styles from "./home.module.scss";
@@ -35,7 +35,7 @@ export default function Home( { product }: HomeProps ) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1LugOiAeASPlznq1b37CBKhi');
 
   const product = {
@@ -50,5 +50,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product
     },
+    revalidate: 60 * 60 * 24, // 1 dia
   }
 }
