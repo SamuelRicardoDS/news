02a4c77dc6447c19f9e787f5cf0549680c54b923
SamuelRicardoDS <samuelricardods@gmail.com>
2022-10-23 09:16:20 -0300

    meu deus, eu tenho que parar com erro bobo

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,24 +1,24 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
+    const {data: session} = useSession();
 
-    const isUserLoggedIn = true;
-
+    console.log(session);
      
-    return isUserLoggedIn ? (
-        <button type="button" className={styles.signInButton}>
+    return session ? (
+        <button type="button" onClick={() => signOut()} className={styles.signInButton}>
             <FaGithub color="#04d361"/>
-          logged
+          Signed in as {session.user.name}
             <FiX color="#737380" className={styles.closeIcon}/>
         </button>
     ) : (
-        <button type="button" className={styles.signInButton}>
+        <button type="button" onClick={() => signIn('github')} className={styles.signInButton}>
             <FaGithub color="#eba417"/>
             Sign in with Github
-            (for granolas only)
         </button>
     )    
 }
